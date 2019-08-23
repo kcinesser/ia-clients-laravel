98a7f1980dfb5a1e3b3d84977bf5ebf13cf8b4d1
Nicolas Esser <nicolas.esser@firespring.com>
2019-08-23 10:26:17 -0500

    Fixed bugs.

--- resources/views/domains/_form.blade.php
+++ resources/views/domains/_form.blade.php
@@ -2,7 +2,7 @@
     <label for="title" class="label text-sm mb-2 block">URL</label>
 
     <div class="control">
-        <input type="text" name="name" value="{{ $domain->name }}" required>
+        <input type="text" name="name" value="" required>
     </div>
 </div>
 
@@ -10,7 +10,7 @@
     <label for="exp_date" class="label text-sm mb-2 block">Expiration Date</label>
 
     <div class="control">
-        <input type="text" name="exp_date" value="{{ $domain->exp_date }}">
+        <input type="text" name="exp_date" value="">
     </div>
 </div>
 
@@ -19,7 +19,7 @@
 
     <select name="registrar_id" id="grid-state">
         @foreach ($registrars as $registrar)
-            <option value="{{ $registrar->id }}" {{ $registrar->id == $domain->registrar_id ? "selected" : "" }}>{{ $registrar->name }}</option>
+            <option value="{{ $registrar->id }}">{{ $registrar->name }}</option>
         @endforeach
     </select>
 </div>
