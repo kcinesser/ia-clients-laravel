459dfb775a9f39287c79f000bf109809d5abcb1f
Jeff Ray <jeff.ray@firespring.com>
2019-08-29 15:39:20 -0500

    Fix model path in use statement

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Models\Cron as Cron;
+use App\Cron as Cron;
 
 class Kernel extends ConsoleKernel
 {
