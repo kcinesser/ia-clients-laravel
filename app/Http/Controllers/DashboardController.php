9b3b794c31cf352cb1c435c60b48caf15ab4b9de
Matia Ward <matia.ward@firespring.com>
2019-09-19 15:09:38 -0500

    fixed error logging

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,8 +18,6 @@ class DashboardController extends Controller
     	$sites = $user->dashboardSites();
     	$updates = Update::all()->sortByDesc('created_at');
 		$activities = Activity::latest()->take(10)->get();
-		
-		Log::critical("TEST CRITICAL ERROR");
 
     	return view('dashboard.dashboard', compact('user', 'clients', 'projects', 'updates', 'activities', 'sites'));
     }
